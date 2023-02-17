525dd9c1fc18505acdfd76855f1b9733ee7738f4
sshodipo <samsho288@gmail.com>
2023-02-16 17:18:03 -0800

    puts string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
-	puts("\n");
+	puts(" \"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
